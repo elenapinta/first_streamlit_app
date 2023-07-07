3f96548db68629126d7b40b4c1b4def69acaaf8d
elenapinta <106950984+elenapinta@users.noreply.github.com>
2023-07-07 11:59:22 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,13 +32,10 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-  #import requests
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  # REMOVED (just writes the data to the screen)--> streamlit.text(fruityvice_response.json())
-  # take the json version of the response and normalize it 
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # output it the screen as a table
   streamlit.dataframe(fruityvice_normalized)
+  
 except URLError as e:
   streamlit.error()
 
